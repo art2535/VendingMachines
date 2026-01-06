43a1fe93f2f01f16f2d2c836f53bd7d826115e31
art2535 <artem010606@yandex.ru>
2026-01-06 22:41:20 +0300

    Обновлена страница авторизации

--- VendingMachines.Web/Pages/Authorization/Auth.cshtml.cs
+++ VendingMachines.Web/Pages/Authorization/Auth.cshtml.cs
@@ -1,14 +1,14 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel.DataAnnotations;
 using System.Text.Json;
 using System.Text;
 using VendingMachines.API.DTOs.Auth;
 using VendingMachines.API.DTOs.Account;
+using VendingMachines.Web.Pages.Base;
 
 namespace VendingMachines.Web.Pages.Authorization
 {
-    public class AuthModel : PageModel
+    public class AuthModel : AuthenticatedPageModel
     {
         private readonly IHttpClientFactory _httpClientFactory;
         private readonly ILogger<AuthModel> _logger;
