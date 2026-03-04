22631e7462503a5075180873ba3dfeee1316c56a
art2535 <artem010606@yandex.ru>
2026-03-04 21:34:27 +0300

    Убраны CORS для взаимодействия с клиентами

--- VendingMachines.API/Program.cs
+++ VendingMachines.API/Program.cs
@@ -16,40 +16,12 @@ namespace VendingMachines.API
 
             builder.Services.AddSwagger();
 
-            builder.Services.AddCors(options =>
-            {
-                options.AddPolicy("AllowWpf", builder =>
-                {
-                    builder.AllowAnyOrigin()
-                        .AllowAnyMethod()
-                        .AllowAnyHeader();
-                });
-                
-                options.AddPolicy("AllowWebApp", policy =>
-                {
-                    policy.WithOrigins("https://localhost:7164")
-                        .AllowAnyHeader()
-                        .AllowAnyMethod();
-                });
-
-                options.AddPolicy("AllowMobile", policy =>
-                {
-                    policy.AllowAnyOrigin()
-                        .AllowAnyMethod()
-                        .AllowAnyHeader();
-                });
-            });
-
             var app = builder.Build();
 
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwaggerInterface();
             }
-
-            app.UseCors("AllowWpf");
-            app.UseCors("AllowWebApp");
-            app.UseCors("AllowMobile");
 
             app.UseAuthentication();
             app.UseAuthorization();
